9c727f9edae4c196f546031f30880092a050bbac
Sean Lin <sean.linxs@gmail.com>
2015-12-01 23:59:56 +1100

    Fix tests

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url
-from . import views
+from main.views.websites import WebsiteList, WebsiteDetails
 
 urlpatterns = [
-    url(r'^owners/$', views.owners, name='owners'),
-    url(r'^owners/create/$', views.create_owner, name="create_owner"),
-    url(r'^owners/(?P<owner_id>[0-9]+)/$', views.owner, name='owner'),
-    url(r'^owners/(?P<owner_id>[0-9]+)/sites/$', views.sites, name='sites'),
-    url(r'^owners/(?P<owner_id>[0-9]+)/sites/(?P<site_id>[0-9]+)/pages/$', views.pages, name='pages'),
-    url(r'^owners/(?P<owner_id>[0-9]+)/sites/(?P<site_id>[0-9]+)/pages/(?P<page_id>[0-9]+)/images/', views.images, name='images'),
+    url(r'^websites/$', WebsiteList.as_view(), name='website_list'),
+    url(r'^websites/(?P<pk>\d+)/$', WebsiteDetails.as_view(), name='website_details'),
 ]
