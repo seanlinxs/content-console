ab065550c86093ed281bfd611f4e91ace55153a4
Sean Lin <sean.linxs@gmail.com>
2015-12-02 00:04:55 +1100

    Revert "Fix tests"
    This reverts commit 9c727f9edae4c196f546031f30880092a050bbac.

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from main.views.websites import WebsiteList, WebsiteDetails
+from . import views
 
 urlpatterns = [
-    url(r'^websites/$', WebsiteList.as_view(), name='website_list'),
-    url(r'^websites/(?P<pk>\d+)/$', WebsiteDetails.as_view(), name='website_details'),
+    url(r'^owners/$', views.owners, name='owners'),
+    url(r'^owners/create/$', views.create_owner, name="create_owner"),
+    url(r'^owners/(?P<owner_id>[0-9]+)/$', views.owner, name='owner'),
+    url(r'^owners/(?P<owner_id>[0-9]+)/sites/$', views.sites, name='sites'),
+    url(r'^owners/(?P<owner_id>[0-9]+)/sites/(?P<site_id>[0-9]+)/pages/$', views.pages, name='pages'),
+    url(r'^owners/(?P<owner_id>[0-9]+)/sites/(?P<site_id>[0-9]+)/pages/(?P<page_id>[0-9]+)/images/', views.images, name='images'),
 ]
