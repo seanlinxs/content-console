0d9c5524ec94cf05489aadadb9b94e7c36a3e846
Sean Lin <sean.linxs@gmail.com>
2015-12-02 00:08:35 +1100

    Unregister admin models

--- main/admin.py
+++ main/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
-from main.models import SiteOwner, Website
 
 # Register your models here.
-
-class WebsiteInline(admin.TabularInline):
-    model = Website
-    extra = 1
-
-
-class SiteOwnerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone')
-    fieldsets = [
-        (None, {'fields': ['name']}),
-        ('Email&Phone', {'fields': ['email', 'phone'], 'classes': ['collapse']}),
-    ]
-    inlines = [WebsiteInline]
-
-
-admin.site.register(SiteOwner, SiteOwnerAdmin)
