e6c7bec7416386980bba97cb88203f1ac92ab3d8
Sean Lin <sean.linxs@gmail.com>
2015-12-13 22:24:09 +1100

    Use project based static folders

--- cc/settings.py
+++ cc/settings.py
@@ -95,7 +95,7 @@ STATICFILES_FINDERS = (
 )
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/var/www/cc/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
